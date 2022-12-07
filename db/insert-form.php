7a672e203fe0736d73609354339df54dcd966432
softanic <info@softanic.in>
2022-12-07 10:07:52 +0530

    db insert

---
+++ db/insert-form.php
@@ -0,0 +1,21 @@
+<html>
+	<body>
+		<form action="insert.php" method="post">
+			<label>Name</lable><br>
+			<input type="text" name="name"><br><br>
+			<label>Mobile No</lable><br>
+			<input type="text" name="mobile_no"><br><br>
+			
+			<label>email id</lable><br>
+			<input type="text" name="email_id"><br><br>
+			
+			<label>password</lable><br>
+			<input type="text" name="password"><br><br>
+			
+			<label>city</lable><br>
+			<input type="text" name="city"><br><br>
+			
+			<input type="submit" ><br><br>
+		</form>
+	</body>
+</html>
---
+++ db/insert.php
@@ -0,0 +1,27 @@
+<?php
+	
+	$conn=mysqli_connect('localhost','root','','php9am');
+	/*if($conn)
+	{
+		echo 'connected';
+	}else{
+		echo 'not connected';
+	}
+	*/
+	$name=$_POST['name'];
+	$mobile_no=$_POST['mobile_no'];
+	$email=$_POST['email_id'];
+	$password=$_POST['password'];
+	$city=$_POST['city'];
+	$q="insert into users values (NULL,'$name','$mobile_no','$email','$password','$city')";
+	$result=mysqli_query($conn,$q);
+	if($result){
+		echo "Data inserted successful";
+	}else{
+		echo "Fail to insert data. Please try again";
+		if(mysqli_errno($conn)==1062)
+		{
+				
+		}
+	}
+?>
